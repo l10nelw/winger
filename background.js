fedb943bbdcad7911a1cf3640ab8fdc5ed758444
Lionel Wong <lionel.wong@easyuni.com>
2019-10-09 21:08:32 +0800

    Commit work so far

---
+++ background.js
@@ -0,0 +1,52 @@
+var POPULATE_TABS = { populate: true, windowTypes: ['normal'] };
+
+var WindowsData = {};
+var LastWindowNumber = 0;
+
+(async () => {
+    await initWindowsData();
+})();
+
+browser.windows.onCreated.addListener(onWindowCreated);
+browser.windows.onRemoved.addListener(onWindowRemoved);
+browser.windows.onFocusChanged.addListener(onWindowFocused);
+
+
+async function initWindowsData() {
+    const allWindows = await browser.windows.getAll(POPULATE_TABS);
+    for (const window of allWindows) {
+        addEntryToWindowsData(window.id);
+    }
+}
+
+function onWindowCreated(window) {
+    const id = window.id;
+    addEntryToWindowsData(id);
+}
+
+function onWindowRemoved(id) {
+    delete WindowsData[id];
+}
+
+function onWindowFocused(id) {
+    if (id > 0) {
+        WindowsData[id].lastFocused = Date.now();
+    }
+}
+
+function addEntryToWindowsData(id) {
+    WindowsData[id] = {};
+    WindowsData[id].lastFocused = Date.now();
+    WindowsData[id].defaultName = `Window ${++LastWindowNumber} / id ${id}`;
+}
+
+// Get tab count
+// ... of current window, when tabs are created/removed // ~window is focused~
+// - tabs.onCreated, tabs.onRemoved
+// - browser.windows.getLastFocused(POPULATE_TABS)
+// - browser.browserAction.setBadgeText({ windowId: id, text: badgeText })
+// ... of source and destination windows, when tabs are moved
+// - tabs.move(tabIds, { windowId: id, index: -1 })
+// - browser.windows.get(id, POPULATE_TABS)
+// - browser.browserAction.setBadgeText({ windowId: id, text: badgeText })
+
---
+++ popup.js
@@ -0,0 +1,67 @@
+(async () => {
+
+    const BgP = await browser.runtime.getBackgroundPage();
+    const $windowList = document.getElementById('windowList');
+    const $rowTemplate = document.getElementById('rowTemplate').content.firstElementChild;
+    main();    
+    
+    async function main() {
+        let allWindows = await browser.windows.getAll(BgP.POPULATE_TABS);
+        let currentWindowId;
+        allWindows.sort(sortLastFocusedDescending);
+        for (const window of allWindows) {
+            if (window.focused) {
+                currentWindowId = window.id;
+            } else {
+                $windowList.appendChild(createRow(window));
+            }
+        }
+        initWindowNamePanel(currentWindowId);
+        $windowList.onclick = onClickRow;
+    }
+
+    function sortLastFocusedDescending(windowA, windowB) {
+        return BgP.WindowsData[windowB.id].lastFocused -
+               BgP.WindowsData[windowA.id].lastFocused;
+    }
+
+    function createRow(window) {
+        const $row = document.importNode($rowTemplate, true);
+        const windowId = window.id;
+        const tabCount = window.tabs.length;
+        const data = BgP.WindowsData[windowId];
+        $row.dataset.id = windowId;
+        $row.querySelector('.windowName').innerText = data.name || data.defaultName;
+        $row.querySelector('.tabCount').innerText = tabCount;
+        return $row;
+    }
+
+    function initWindowNamePanel(id) {
+        const $windowNameInput = document.getElementById('windowNameInput');
+        // const $windowNameBtn = document.getElementById('windowNameBtn');
+        const data = BgP.WindowsData[id];
+        $windowNameInput.value = data.name || data.defaultName;
+        $windowNameInput.onfocus = $windowNameInput.select;
+        // $windowNameBtn.onclick = () => {
+        //     setWindowName(id, $windowNameInput.value);
+        //     window.close();
+        // };
+    }
+
+    async function onClickRow(e) {
+        const $row = e.target.closest('tr');
+        if ($row) {
+            const id = parseInt($row.dataset.id);
+            await browser.windows.update(id, { focused: true });
+        }
+    }
+
+
+
+    // function setWindowName(id, name) {
+    //     browser.windows.update(id, {
+    //         titlePreface: `${name} - `,
+    //     });
+    // }
+    
+})()
