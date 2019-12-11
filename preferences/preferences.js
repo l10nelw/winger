9ecc3fd8ba95f8ae356d68835f332fc0bcad068c
Lionel Wong <lionelster@gmail.com>
2019-12-11 15:08:18 +0800

    Rename options.html/js to preferences.html/js

---
+++ preferences/preferences.js
@@ -0,0 +1,22 @@
+const $form = document.querySelector('form');
+$form.onchange = set;
+
+function set(event) {
+    const $target = event.target;
+    const name = $target.name;
+    const setting = {};
+    setting[name] = getValue(name, $target);
+    browser.storage.local.set(setting);
+    console.log(setting);
+}
+
+function getValue(name, $target) {
+    const $field = $form[name];
+    if ($target.type == 'checkbox') {
+        if ($field.length) {
+            return [...$field].filter($i => $i.checked).map($i => $i.value); // array
+        }
+        return $field.checked; // boolean
+    }
+    return $field.value; // string
+}
