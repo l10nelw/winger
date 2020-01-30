a06c221860feeea555de0e288cf35e71fe961f9a
Lionel Wong <lionelster@gmail.com>
2020-01-30 15:24:36 +0800

    Update tooltips.js

--- popup/tooltip.js
+++ popup/tooltip.js
@@ -2,12 +2,11 @@ import { hasClass, getModifiers, end } from '../utils.js';
 import * as Popup from './popup.js';
 import * as Status from './status.js';
 
-let bringModifier, sendModifier, EditTooltips, Tooltips, $lastTarget;
+let bring_modifier, send_modifier, EditTooltips, Tooltips, $lastTarget;
 
 export function generate(tabCount, hasReopenTab) {
-    bringModifier = Popup.OPTIONS.bring_modifier;
-    sendModifier = Popup.OPTIONS.send_modifier;
-    const tabPhrase = tabCount == 1 ? 'tab' : `${tabCount} tabs`;
+    ( { bring_modifier, send_modifier } = Popup.OPTIONS );
+    const tabPhrase = tabCount == 1 ? '1 tab' : `${tabCount} tabs`;
 
     EditTooltips = {
         doneEdit: {
@@ -15,19 +14,19 @@ export function generate(tabCount, hasReopenTab) {
             match: ($target, $row) => hasClass('editModeRow', $row) && hasClass('editBtn', $target),
         },
         edit: {
-            text: `Edit:`,
+            text: `Edit `,
             match: ($target) => hasClass('editBtn', $target),
         },
     };
 
     const ReopenTooltips = hasReopenTab ? {
         bringReopenTab: {
-            text: `Bring (close and reopen) ${tabPhrase} to:`,
+            text: `Bring (close-reopen) ${tabPhrase} to `,
             match: ($target, $otherRow, doBringTab, doSendTab) =>
                 hasClass('reopenTab', $otherRow) && (doBringTab || !doSendTab && hasClass('bringBtn', $target)),
         },
         sendReopenTab: {
-            text: `Send (close and reopen) ${tabPhrase} to:`,
+            text: `Send (close-reopen) ${tabPhrase} to `,
             match: ($target, $otherRow, _, doSendTab) =>
                 hasClass('reopenTab', $otherRow) && (doSendTab || hasClass('sendBtn', $target)),
         },
@@ -36,16 +35,16 @@ export function generate(tabCount, hasReopenTab) {
     Tooltips = {
         ...ReopenTooltips,
         bringTab: {
-            text: `Bring ${tabPhrase} to:`,
+            text: `Bring ${tabPhrase} to `,
             match: ($target, $otherRow, doBringTab, doSendTab) =>
                 doBringTab && $otherRow || !doSendTab && hasClass('bringBtn', $target),
         },
         sendTab: {
-            text: `Send ${tabPhrase} to:`,
+            text: `Send ${tabPhrase} to `,
             match: ($target, $otherRow, _, doSendTab) => doSendTab && $otherRow || hasClass('sendBtn', $target),
         },
         switch: {
-            text: `Switch to:`,
+            text: `Switch to `,
             match: (_, $otherRow) => $otherRow,
         }
     };
@@ -64,18 +63,18 @@ export function show(event) {
         $target = $lastTarget;
         modifiers = event;
     }
-    if (!$target.closest('.action')) return Status.show();
+    if (!$target || !$target.closest('.action')) return Status.show();
     let text = matchTooltip(EditTooltips, $target, $target.$row);
     if (!text) {
         modifiers = modifiers || getModifiers(event);
         const $otherRow = $target.closest('.otherRow');
-        const doBringTab = modifiers.includes(bringModifier);
-        const doSendTab  = modifiers.includes(sendModifier);
+        const doBringTab = modifiers.includes(bring_modifier);
+        const doSendTab  = modifiers.includes(send_modifier);
         text = matchTooltip(Tooltips, $target, $otherRow, doBringTab, doSendTab);
-        const modifierText = doBringTab ? `[${bringModifier}] ` : doSendTab ? `[${sendModifier}] ` : ``;
+        const modifierText = doBringTab ? `[${bring_modifier}] ` : doSendTab ? `[${send_modifier}] ` : ``;
         text = modifierText + text;
     }
-    if (end(text) == ':') text += ` ${Popup.rowName($target.$row || $target)}`;
+    if (end(text) == ' ') text += Popup.rowName($target.$row || $target);
     Status.show(text);
 }
 
