aec4e795440bafd442ec5cfca9977b3cc0230e88
Lionel Wong <lionelster@gmail.com>
2020-02-04 15:50:41 +0800

    Update utils.js, convert to arrow functions

--- utils.js
+++ utils.js
@@ -1,16 +1,9 @@
-export function hasClass(cls, $element) {
-    return $element && $element.classList.contains(cls);
-}
+export const hasClass = (cls, $element) => $element && $element.classList.contains(cls);
 
-export function sum(array) {
-    return array.reduce((a, b) => a + b, 0);
-}
+export const sum = array => array.reduce((a, b) => a + b, 0);
 
-export function end(sequence) {
-    return sequence[sequence.length - 1];
-}
+export const end = sequence => sequence[sequence.length - 1];
 
 // Return array of active modifiers in the form of ['Alt', 'Ctrl', 'Shift'] or parts thereof or [].
-export function getModifiers(event) {
-    return ['altKey', 'ctrlKey', 'shiftKey'].filter(m => event[m]).map(m => m[0].toUpperCase() + m.slice(1, -3));
-}
+export const getModifiers =
+    event => ['altKey', 'ctrlKey', 'shiftKey'].filter(m => event[m]).map(m => m[0].toUpperCase() + m.slice(1, -3));
