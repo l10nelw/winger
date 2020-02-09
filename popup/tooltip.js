aff439a68fdf19c176515974c7a487361349ef1d
Lionel Wong <lionelster@gmail.com>
2020-02-09 22:05:06 +0800

    Add tooltip to element title; Improve documentation

--- popup/tooltip.js
+++ popup/tooltip.js
@@ -14,19 +14,19 @@ export function generate(tabCount, hasReopenTab) {
             match: ($target, $row) => hasClass('editModeRow', $row) && hasClass('editBtn', $target),
         },
         edit: {
-            text: `Edit `,
+            text: `Edit: `,
             match: ($target) => hasClass('editBtn', $target),
         },
     };
 
     const ReopenTooltips = hasReopenTab ? {
         bringReopenTab: {
-            text: `Bring (close-reopen) ${tabPhrase} to `,
+            text: `Bring (close-reopen) ${tabPhrase} to: `,
             match: ($target, $otherRow, doBringTab, doSendTab) =>
                 hasClass('reopenTab', $otherRow) && (doBringTab || !doSendTab && hasClass('bringBtn', $target)),
         },
         sendReopenTab: {
-            text: `Send (close-reopen) ${tabPhrase} to `,
+            text: `Send (close-reopen) ${tabPhrase} to: `,
             match: ($target, $otherRow, _, doSendTab) =>
                 hasClass('reopenTab', $otherRow) && (doSendTab || hasClass('sendBtn', $target)),
         },
@@ -35,23 +35,23 @@ export function generate(tabCount, hasReopenTab) {
     Tooltips = {
         ...ReopenTooltips,
         bringTab: {
-            text: `Bring ${tabPhrase} to `,
+            text: `Bring ${tabPhrase} to: `,
             match: ($target, $otherRow, doBringTab, doSendTab) =>
                 doBringTab && $otherRow || !doSendTab && hasClass('bringBtn', $target),
         },
         sendTab: {
-            text: `Send ${tabPhrase} to `,
+            text: `Send ${tabPhrase} to: `,
             match: ($target, $otherRow, _, doSendTab) => doSendTab && $otherRow || hasClass('sendBtn', $target),
         },
         switch: {
-            text: `Switch to `,
+            text: `Switch to: `,
             match: (_, $otherRow) => $otherRow,
         }
     };
 }
 
 // Show tooltip based on event.
-// A modifer array may be given instead of event to toggle relevant modifier text in the tooltip.
+// A modifer array may be given instead of event to include relevant modifier text in the tooltip.
 export function show(event) {
     let modifiers;
     let $target = event.target;
@@ -64,21 +64,23 @@ export function show(event) {
         modifiers = event;
     }
     if (!$target || !$target.closest('.action')) return Status.show();
-    let text = matchTooltip(EditTooltips, $target, $target.$row);
+    let text = select(EditTooltips, $target, $target.$row);
     if (!text) {
         modifiers = modifiers || getModifiers(event);
         const $otherRow = $target.closest('.otherRow');
         const doBringTab = modifiers.includes(bring_modifier);
         const doSendTab  = modifiers.includes(send_modifier);
-        text = matchTooltip(Tooltips, $target, $otherRow, doBringTab, doSendTab);
+        text = select(Tooltips, $target, $otherRow, doBringTab, doSendTab);
         const modifierText = doBringTab ? `[${bring_modifier}] ` : doSendTab ? `[${send_modifier}] ` : ``;
         text = modifierText + text;
     }
     if (end(text) == ' ') text += Popup.rowName($target.$row || $target);
     Status.show(text);
+    $target.title = text;
 }
 
-function matchTooltip(tooltipDict, ...args) {
+// Select tooltip from a dict based on args.
+function select(tooltipDict, ...args) {
     for (const action in tooltipDict) {
         const tooltip = tooltipDict[action];
         if (tooltip.match(...args)) return tooltip.text;
