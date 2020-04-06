d64f3bfa61ce1fa77661e2beecf32cc0d5ffceba
Lionel Wong <lionelster@gmail.com>
2020-04-06 15:29:33 +0800

    Update options page

--- options/options.js
+++ options/options.js
@@ -3,16 +3,13 @@ import * as Settings from '../background/settings.js';
 const $body = document.body;
 const $form = $body.querySelector('form');
 let SETTINGS;
-init();
-
-async function init() {
+(async () => {
     SETTINGS = await Settings.retrieve();
     for (const fieldName in SETTINGS) {
         setFieldValue(fieldName, SETTINGS[fieldName]);
     }
-    $body.hidden = false; // Page initially hidden to avoid flash of value changes on reload.
     $form.onchange = onFieldChange;
-}
+})();
 
 function onFieldChange(event) {
     const $target = event.target;
