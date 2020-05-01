b04c3261a584313371ecc166154b1e5a6fc04683
Lionel Wong <lionelster@gmail.com>
2020-05-01 11:37:05 +0800

    Disable apply buttons until form has changes

--- options/options.js
+++ options/options.js
@@ -1,36 +1,48 @@
 import * as Settings from '../background/settings.js';
 
 const $form = document.body.querySelector('form');
+const $fields = [...$form.querySelectorAll('input')];
+const $submitBtns = [...$form.querySelectorAll('button')];
 const relevantProp = $field => $field.type === 'checkbox' ? 'checked' : 'value';
-let SETTINGS;
+const getFormValuesString = () => $fields.map($field => $field[relevantProp($field)]).join();
+let SETTINGS, formData;
 
 (async () => {
     SETTINGS = await Settings.retrieve();
-    for (const $field of $form.elements) {
+    for (const $field of $fields) {
         loadSetting($field);
         setRelatedFieldAccess($field);
     }
+    formData = getFormValuesString();
 })();
 
 $form.onchange = onFieldChange;
-$form.onsubmit = browser.runtime.reload;
+$form.onsubmit = onFormSubmit;
 
 function onFieldChange(event) {
-    const $field = event.target;
-    saveSetting($field);
-    setRelatedFieldAccess($field);
+    setRelatedFieldAccess(event.target);
+    updateSubmitBtns();
 }
 
-function saveSetting($field) {
-    const value = $field[relevantProp($field)];
-    browser.storage.local.set({ [$field.name]: value });
+function onFormSubmit() {
+    $fields.forEach(saveSetting);
+    browser.runtime.reload();
 }
 
 function loadSetting($field) {
     $field[relevantProp($field)] = SETTINGS[$field.name];
 }
 
+function saveSetting($field) {
+    browser.storage.local.set({ [$field.name]: $field[relevantProp($field)] });
+}
+
 function setRelatedFieldAccess($field) {
     const $related = $form[$field.dataset.enables];
     if ($related) $related.disabled = !$field.checked;
+}
+
+function updateSubmitBtns() {
+    const isFormUnchanged = formData === getFormValuesString();
+    $submitBtns.forEach($btn => $btn.disabled = isFormUnchanged);
 }
