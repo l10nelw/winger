45c26612ef749ed2bdceb06bbba63ac943998d1f
Lionel Wong <lionelster@gmail.com>
2020-05-02 20:39:23 +0800

    Update help; Improve script

--- help/help.js
+++ help/help.js
@@ -1,10 +1,16 @@
-const $$ = selector => document.body.querySelectorAll(selector);
-$$('.js-version').forEach($el => $el.textContent = browser.runtime.getManifest().version);
-browser.commands.getAll().then(commands => {
-    $$('.js-shortcut').forEach($el => $el.textContent = commands[0].shortcut);
-}).then(() => {
+(async ()=>{
+
+    const $$ = selector => document.body.querySelectorAll(selector);
+
+    const { version } = browser.runtime.getManifest();
+    $$('.js-version').forEach($el => $el.textContent = version);
+
+    const [{ shortcut }] = await browser.commands.getAll();
+    $$('.js-shortcut').forEach($el => $el.textContent = shortcut);
+
     if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
         $$('.js-cmdOnMac').forEach($el => $el.textContent = $el.textContent.replace('Ctrl', 'Cmd'));
-        $$('.js-hideOnMac').forEach($el => $el.style.visibility = 'hidden');
+        document.styleSheets[0].insertRule(`.js-hideOnMac { visibility: hidden }`);
     }
-});
+
+})();
