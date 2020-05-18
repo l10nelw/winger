2f9ab6a0e3760084d01a27b8e9c92060e3b84808
Lionel Wong <lionelster@gmail.com>
2020-05-18 21:01:42 +0800

    Add sticky sidebar to help page
    WIP

--- help/help.js
+++ help/help.js
@@ -1,7 +1,12 @@
 (async ()=>{
 
-    const $$ = selector => document.body.querySelectorAll(selector);
+    const $body = document.body;
+    const $ = (selector, $scope = $body) => $scope.querySelector(selector);
+    const $$ = (selector, $scope = $body) => $scope.querySelectorAll(selector);
 
+    const $aside = $('aside');
+    $$('h1, h2, h3').forEach($heading => $aside.appendChild($heading.cloneNode(true)));
+    
     const { version } = browser.runtime.getManifest();
     $$('.js-version').forEach($el => $el.textContent = version);
 
