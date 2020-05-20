4abe31d3a985a919ddc99786276d15a1af0d79d3
Lionel Wong <lionelster@gmail.com>
2020-05-20 21:04:17 +0800

    Refactor help.js; Add Windows OS detection

--- help/help.js
+++ help/help.js
@@ -1,21 +1,44 @@
-(async ()=>{
+const $body = document.body;
+const $ = (selector, $scope = $body) => $scope.querySelector(selector);
+const $$ = (selector, $scope = $body) => $scope.querySelectorAll(selector);
 
-    const $body = document.body;
-    const $ = (selector, $scope = $body) => $scope.querySelector(selector);
-    const $$ = (selector, $scope = $body) => $scope.querySelectorAll(selector);
-
-    const $aside = $('aside');
-    $$('h1, h2, h3').forEach($heading => $aside.appendChild($heading.cloneNode(true)));
+populateSidebar();
+insertVersion();
+insertShortcut();
+doOSSpecific();
     
+function populateSidebar() {
+    const $aside = $('aside');
+    $$('h2, h3', $('main')).forEach($h => {
+        $h = $h.cloneNode(true);
+        $h.className = '';
+        $aside.appendChild($h);
+    });
+}
+
+function insertVersion() {
     const { version } = browser.runtime.getManifest();
     $$('.js-version').forEach($el => $el.textContent = version);
+}
 
+async function insertShortcut() {
     const [{ shortcut }] = await browser.commands.getAll();
     $$('.js-shortcut').forEach($el => $el.textContent = shortcut);
+}
 
-    if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
+function doOSSpecific() {
+    const isOS = str => navigator.userAgent.indexOf(str) !== -1;
+    const addCSS = rule => document.styleSheets[0].insertRule(rule);
+
+    if (isOS('Mac OS X')) {
         $$('.js-cmdOnMac').forEach($el => $el.textContent = $el.textContent.replace('Ctrl', 'Cmd'));
-        document.styleSheets[0].insertRule(`.js-hideOnMac { visibility: hidden }`);
+        addCSS(`.js-hideOnMac { visibility: hidden }`);
+    } else {
+        addCSS(`.js-showOnMac { visibility: hidden }`);
     }
 
-})();
+    if (isOS('Windows')) {
+    } else {
+        addCSS(`.js-showOnWin { visibility: hidden }`);
+    }
+}
