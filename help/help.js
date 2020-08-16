9cc0fbf28641e1d2950af550b3bbae8ca384dfa5
Lionel Wong <lionelster@gmail.com>
2020-08-16 14:07:17 +0800

    Replace images of popup in help with html/css

--- help/help.js
+++ help/help.js
@@ -1,16 +1,45 @@
-(async ()=>{
+const $body = document.body;
+const $ = (selector, $scope = $body) => $scope.querySelector(selector);
+const $$ = (selector, $scope = $body) => $scope.querySelectorAll(selector);
 
-    const $$ = selector => document.body.querySelectorAll(selector);
+insertVersion();
+insertShortcut();
+doOSSpecific();
+updateMockPopups();
 
+function insertVersion() {
     const { version } = browser.runtime.getManifest();
     $$('.js-version').forEach($el => $el.textContent = version);
+}
 
+async function insertShortcut() {
     const [{ shortcut }] = await browser.commands.getAll();
     $$('.js-shortcut').forEach($el => $el.textContent = shortcut);
+}
 
-    if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
+function doOSSpecific() {
+    const isOS = str => navigator.userAgent.indexOf(str) !== -1;
+    const isMac = isOS('Mac OS X');
+    const isWin = isOS('Windows');
+
+    const addCSS = rule => document.styleSheets[0].insertRule(rule);
+    addCSS(`.js-${isMac ? 'hide' : 'show'}OnMac { visibility: hidden }`);
+    addCSS(`.js-${isWin ? 'hide' : 'show'}OnWin { visibility: hidden }`);
+
+    if (isMac) {
         $$('.js-cmdOnMac').forEach($el => $el.textContent = $el.textContent.replace('Ctrl', 'Cmd'));
-        document.styleSheets[0].insertRule(`.js-hideOnMac { visibility: hidden }`);
     }
+}
 
-})();
+function updateMockPopups() {
+    $$('.popup').forEach($popup => {
+        const $status = $('.popup-status', $popup);
+        if (!$status) return;
+        const statusText = $status.textContent;
+        if (!statusText.includes('#')) return;
+        const $tabCounts = [...$$('.popup-tabCount', $popup)];
+        const tabCount = $tabCounts.reduce((total, $el) => total + parseInt($el.textContent), 0);
+        const windowCount = $tabCounts.length;
+        $status.textContent = statusText.replace('#', tabCount).replace('#', windowCount);
+    });
+}
