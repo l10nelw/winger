bfe2f1b78bdfd978b4f49fd0286869db332f213c
Lionel Wong <lionelster@gmail.com>
2020-11-24 21:56:49 +0800

    Refactor Menu module: group up windows-submenu functions

--- background/menu.js
+++ background/menu.js
@@ -2,64 +2,93 @@ import * as Modifier from '../modifier.js';
 import * as Metadata from './metadata.js';
 import * as WindowTab from './windowtab.js';
 
-let contextsEnabled;
 const contextTitle = {
     tab:  'Send Tab to &Window',
     link: 'Open Link in &Window',
 }
 
-// Create a parent menu item for each given context.
+const windowsSubmenu = {
+
+    enabledContexts: [],
+    usedBy: context => ['tab', 'link'].includes(context),
+
+    init(context) {
+        this.enabledContexts.push(context);
+        // Add dummy submenu item to avoid parent menu resizing onShown:
+        const id = this._menuId(context);
+        browser.menus.create({ contexts: [context], parentId: context, id, title: '-' });
+    },
+
+    // Update menu state based on window count.
+    updateEnabled() {
+        const properties = { enabled: Metadata.windowCount > 1 };
+        this.enabledContexts.forEach(context => browser.menus.update(context, properties));
+    },
+
+    // Clear and populate `context` menu with other-window menu items, sorted by lastFocsued.
+    populate(context, currentWindowId) {
+        const properties = { contexts: [context], parentId: context };
+        for (const { id: windowId } of Metadata.sorted()) {
+            const menuId = this._menuId(context, windowId);
+            browser.menus.remove(menuId);
+            if (windowId == currentWindowId) continue;
+            browser.menus.create({ ...properties, id: menuId, title: Metadata.getName(windowId) });
+        }
+        browser.menus.remove(this._menuId(context)); // Remove dummy if it exists
+        browser.menus.refresh();
+    },
+
+    openLink(url, windowId, modifiers) {
+        browser.tabs.create({ windowId, url });
+        if (modifiers.includes(Modifier.BRING)) WindowTab.switchWindow(windowId);
+    },
+
+    async moveTab(tab, windowId, modifiers, originWindowId) {
+        const tabs = tab.highlighted ? await WindowTab.getSelectedTabs() : [tab];
+        WindowTab.doAction({ action: 'send', windowId, originWindowId, modifiers, tabs });
+    },
+
+    _menuId: (context, windowId) => `${windowId || ''}-${context}`,
+
+};
+
+// Create a menu item for each given context.
 export function init(contextList) {
-    contextsEnabled = contextList;
-    for (const context of contextsEnabled) {
-        const contexts = [context];
-        browser.menus.create({ contexts, id: context, title: contextTitle[context] });
-        browser.menus.create({ contexts, parentId: context, id: `-${context}`, title: '-' }); // Dummy to avoid menu resizing onShown
+    for (const context of contextList) {
+        createMenuItem(context);
+        if (windowsSubmenu.usedBy(context)) windowsSubmenu.init(context);
     }
-    update();
+    windowsSubmenu.updateEnabled();
     browser.menus.onShown.addListener   (onMenuShow);
     browser.menus.onClicked.addListener (onMenuClick);
 }
 
-function onMenuShow(info, tab) {
-    if (!tab) return;
-    const context = info.contexts.includes('link') ? 'link' : 'tab';
-    populate(context, tab.windowId);
-    browser.menus.refresh();
+export function update() {
+    windowsSubmenu.updateEnabled();
+}
+
+function createMenuItem(context) {
+    browser.menus.create({ contexts: [context], id: context, title: contextTitle[context] });
+}
+
+async function onMenuShow(info, tab) {
+    // Populate windows submenu if applicable
+    const contexts = info.contexts;
+    const context =
+        contexts.includes('link') ? 'link' :
+        contexts.includes('tab') ? 'tab' :
+        null;
+    if (!context) return;
+    if (windowsSubmenu.enabledContexts.includes(context)) {
+        windowsSubmenu.populate(context, tab.windowId);
+    }
 }
 
 function onMenuClick(info, tab) {
+    // Windows submenu item
     const windowId = parseInt(info.menuItemId);
     if (!windowId) return;
     const url = info.linkUrl;
-    url ? openLink (url, windowId, info.modifiers)
-        : moveTab  (tab, windowId, info.modifiers, tab.windowId);
-}
-
-// Update menu visibility based on window count.
-export function update() {
-    const props = { visible: Metadata.windowCount > 1 };
-    contextsEnabled.forEach(context => browser.menus.update(context, props));
-}
-
-// Clear and populate `context` menu with other-window menu items, sorted by lastFocsued.
-function populate(context, currentWindowId) {
-    const props = { contexts: [context], parentId: context };
-    for (const { id: windowId } of Metadata.sorted()) {
-        const menuId = `${windowId}-${context}`;
-        browser.menus.remove(menuId);
-        if (windowId == currentWindowId) continue;
-        browser.menus.create({ ...props, id: menuId, title: Metadata.getName(windowId) });
-    }
-    browser.menus.remove(`-${context}`); // Remove dummy if it exists
-}
-
-function openLink(url, windowId, modifiers) {
-    browser.tabs.create({ windowId, url });
-    if (modifiers.includes(Modifier.BRING)) WindowTab.switchWindow(windowId);
-}
-
-async function moveTab(tab, windowId, modifiers, originWindowId) {
-    const tabs = tab.highlighted ? await WindowTab.getSelectedTabs() : [tab];
-    WindowTab.doAction({ action: 'send', windowId, originWindowId, modifiers, tabs });
+    url ? windowsSubmenu.openLink (url, windowId, info.modifiers)
+        : windowsSubmenu.moveTab  (tab, windowId, info.modifiers, tab.windowId);
 }
