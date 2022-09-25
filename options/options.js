885a89bf11d309ab79ad7c9eda31a763ed6d973e
Lionel Wong <lionelster@gmail.com>
2022-09-26 07:00:14 +0800

    Refactor options.js: namespace related objects
    Rename enablerMap.trigger

--- options/options.js
+++ options/options.js
@@ -4,127 +4,129 @@ import { getShortcut, GroupMap } from '../utils.js';
 import { validify } from '../background/name.js';
 import { openHelp } from '../background/action.js';
 
-const $body = document.body;
-const $form = $body.querySelector('form');
-const $settingFields = [...$form.querySelectorAll('.setting')];
-const stash_subSymbol = $form.stash_home.options[1].text.slice(-1);
-const enablerMap = new GroupMap(); // Fields that enable/disable other fields
+const $form = document.body.querySelector('form');
+
+const settings = {
+    $fields: [...$form.querySelectorAll('.setting')],
+    _relevantProp: type => (type === 'checkbox') ? 'checked' : 'value',
+
+    //@ (Boolean|String, Object) -> state
+    load(value, $field) {
+        $field[this._relevantProp($field.type)] = value;
+    },
+
+    //@ state -> state
+    saveAll() {
+        const newSettings = {};
+        for (const $field of this.$fields) {
+            newSettings[$field.name] = $field[this._relevantProp($field.type)];
+        }
+        Settings.set(newSettings);
+        browser.runtime.reload();
+    },
+};
+
+// Checkboxes that enable/disable other fields (targets)
+const enablerMap = Object.assign(new GroupMap(), {
+
+    //@ (Object, Boolean) -> state
+    _updateTarget($target, disable) {
+        $target.disabled = disable;
+        $target.closest('label')?.classList.toggle('muted', disable);
+    },
+
+    //@ (Object), state -> state
+    addTarget($target) {
+        const $enabler = $form[$target.dataset.enabledBy];
+        if (!$enabler)
+            return;
+        this.group($target, $enabler);
+        this._updateTarget($target, $enabler.disabled || !$enabler.checked);
+    },
+
+    // Enable/disable fields that $enabler controls.
+    //@ (Object), state -> state|null
+    trigger($enabler) {
+        const $targets = this.get($enabler);
+        if (!$targets)
+            return;
+        // Disable targets if enabler is unchecked or is itself disabled
+        const disable = $enabler.disabled || !$enabler.checked;
+        for (const $target of $targets) {
+            this._updateTarget($target, disable);
+            this.trigger($target); // In case $target is itself an enabler
+        }
+    },
+});
+
+const stashSection = {
+    permission: { permissions: ['bookmarks'] },
+    subfolderSymbol: $form.stash_home.options[1].text.slice(-1),
+
+    //@ (Object), state -> state|null
+    async onEnabled($field) {
+        if ($field !== $form.enable_stash)
+            return;
+        if (!$field.checked)
+            return browser.permissions.remove(this.permission);
+        $field.checked = await browser.permissions.request(this.permission);
+    },
+
+    // Add/update subfolder name in the stash home <select>.
+    //@ state -> state
+    updateHomeSelect() {
+        const name = $form.stash_home_name.value = validify($form.stash_home_name.value);
+        const isSubfolder = $option => !$option.value.endsWith('_');
+        for (const $option of $form.stash_home.options) {
+            if (isSubfolder($option))
+                $option.text = `${$option.previousElementSibling.text} ${this.subfolderSymbol} ${name}`;
+        }
+    },
+};
+
+const staticText = {
+
+    //@ state -> state
+    async insertShortcut() {
+        const defaultShortcut = browser.runtime.getManifest().commands._execute_browser_action.suggested_key.default;
+        const currentShortcut = await getShortcut();
+        if (currentShortcut)
+            $form.querySelector('.current-shortcut').textContent = currentShortcut;
+        if (currentShortcut == defaultShortcut)
+            return;
+        const $defaultShortcutText = $form.querySelector('.default-shortcut-text');
+        $defaultShortcutText.querySelector('.default-shortcut').textContent = defaultShortcut;
+        $defaultShortcutText.hidden = false;
+    },
+
+    //@ state -> state
+    async checkPrivateAccess() {
+        const isAllowed = await browser.extension.isAllowedIncognitoAccess();
+        const $toShow = $form.querySelectorAll(`.private-allowed-${isAllowed ? 'yes' : 'no'}`);
+        $toShow.forEach($el => $el.hidden = false);
+    },
+};
 
 (async function init() {
     const SETTINGS = await browser.runtime.sendMessage({ settings: true });
     Theme.apply(SETTINGS.theme);
-    for (const $field of $settingFields) {
-        loadSetting($field);
-        registerEnabler($field);
+    for (const $field of settings.$fields) {
+        settings.load(SETTINGS[$field.name], $field);
+        enablerMap.addTarget($field);
     }
-    stash_updateHomeSelect();
-    staticText_insertShortcut();
-    staticText_checkPrivateAccess();
-    $form.onchange = onFieldChange;
-    $form.onclick = onElClick;
-    $form.onsubmit = saveSettings;
-
-    //@ (Object), state -> state
-    function loadSetting($field) {
-        const value = SETTINGS[$field.name];
-        const type = $field.type;
-        if (type === 'radio') {
-            $field.checked = $field.value === value;
-        } else {
-            $field[type === 'checkbox' ? 'checked' : 'value'] = value;
-        }
-    }
-
-    //@ (Object), state -> state
-    function registerEnabler($field) {
-        const $enabler = $form[$field.dataset.enabledBy];
-        if ($enabler) {
-            enablerMap.group($field, $enabler);
-            updateEnablerTarget($field, $enabler.disabled || !$enabler.checked);
-        }
-    }
+    stashSection.updateHomeSelect();
+    staticText.insertShortcut();
+    staticText.checkPrivateAccess();
 })();
 
-//@ ({ Object }), state -> state
-async function onFieldChange({ target: $field }) {
-    await stash_onChecked($field);
-    stash_updateHomeSelect();
-    activateEnabler($field);
-}
+$form.addEventListener('change', async ({ target: $field }) => {
+    await stashSection.onEnabled($field);
+    enablerMap.trigger($field);
+});
 
-//@ ({ Object }), state -> state|null
-function onElClick({ target: $el }) {
+$form.addEventListener('click', ({ target: $el }) => {
     if ($el.classList.contains('help'))
-        openHelp($el.getAttribute('href'));
-}
+        return openHelp($el.getAttribute('href'));
+});
 
-//@ state -> state
-function saveSettings() {
-    const newSettings = {};
-    for (const $field of $settingFields) {
-        const type = $field.type;
-        if (type === 'radio') {
-            if ($field.checked) {
-                newSettings[$field.name] = $field.value;
-            }
-        } else {
-            newSettings[$field.name] = $field[type === 'checkbox' ? 'checked' : 'value'];
-        }
-    }
-    Settings.set(newSettings);
-    browser.runtime.reload();
-}
-
-// Enable/disable fields that $enabler controls.
-//@ (Object), state -> state|null
-function activateEnabler($enabler) {
-    const $targets = enablerMap.get($enabler);
-    if (!$targets) return;
-    const disable = $enabler.disabled || !$enabler.checked; // Disable targets if enabler is unchecked or is itself disabled
-    for (const $target of $targets) {
-        updateEnablerTarget($target, disable);
-        activateEnabler($target); // In case $target is itself an enabler
-    }
-}
-
-//@ (Object, Boolean) -> state
-function updateEnablerTarget($field, disable) {
-    $field.disabled = disable;
-    $field.closest('label')?.classList.toggle('muted', disable);
-}
-
-//@ (Object), state -> state|null
-async function stash_onChecked($field) {
-    if ($field !== $form.enable_stash) return;
-    const permission = { permissions: ['bookmarks'] };
-    if (!$field.checked) return browser.permissions.remove(permission);
-    $field.checked = await browser.permissions.request(permission);
-}
-
-// Add/update subfolder name in the stash home <select>.
-//@ state -> state
-function stash_updateHomeSelect() {
-    const name = validify($form.stash_home_name.value);
-    $form.stash_home_name.value = name;
-    for (const $option of $form.stash_home.options)
-        if (!$option.value.endsWith('_'))
-            $option.text = `${$option.previousElementSibling.text} ${stash_subSymbol} ${name}`;
-}
-
-//@ state -> state
-async function staticText_insertShortcut() {
-    const defaultShortcut = browser.runtime.getManifest().commands._execute_browser_action.suggested_key.default;
-    const currentShortcut = await getShortcut();
-    if (currentShortcut) $body.querySelector('.current-shortcut').textContent = currentShortcut;
-    if (currentShortcut == defaultShortcut) return;
-    const $defaultShortcutText = $body.querySelector('.default-shortcut-text');
-    $defaultShortcutText.querySelector('.default-shortcut').textContent = defaultShortcut;
-    $defaultShortcutText.hidden = false;
-}
-
-//@ state -> state
-async function staticText_checkPrivateAccess() {
-    const isAllowed = await browser.extension.isAllowedIncognitoAccess();
-    const $toShow = $body.querySelectorAll(`.private-allowed-${isAllowed ? 'yes' : 'no'}`);
-    $toShow.forEach($el => $el.hidden = false);
-}
+$form.addEventListener('submit', settings.saveAll);
