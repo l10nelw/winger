b2d00872d6d1933a6feae72f5cb517b43dffdb51
Lionel Wong <lionelster@gmail.com>
2022-10-28 15:02:33 +0800

    Revert error field when defocused; Cleanup editmode
    - Replace $focusedName with document.activeElement
    - Simplify toggleNameFields
    - Fix handleKeyUp sometimes returning false while editmode active

--- popup/editmode.js
+++ popup/editmode.js
@@ -11,15 +11,15 @@ import * as Request from './request.js';
 const HINT = `Edit Mode: ENTER/↑/↓ to Save, ESC to Cancel`;
 
 export let isActive = false; // Indicates if popup is in Edit Mode
-let $names, $actions, $focusedName;
+let $names, $actions;
 
 //@ (Object), state -> state
 export function activate($name = $currentWindowRow.$name) {
-    $names ??= $body.querySelectorAll('.name');
+    $names = [...$body.querySelectorAll('.name')];
     $actions ??= $body.querySelectorAll('[data-action]');
     setActive(true);
     $name.focus();
-    $focusedName = $name;
+    $name._original = $name.value; // Remember name at focus time
 }
 
 //@ -> state
@@ -41,13 +41,9 @@ function setActive(isActivate) {
 
 //@ (Object, Object) -> (Boolean), state|nil
 export function handleFocusIn($focused, $defocused) {
-    if (!isActive) return false;
+    if (!isActive)
+        return false;
 
-    // Disallow defocusing field with error
-    if ($defocused.classList.contains('nameError')) {
-        $defocused.focus();
-        return true;
-    }
     // Disallow focus on non-fields
     if (!isField($focused)) {
         $defocused.focus();
@@ -57,12 +53,16 @@ export function handleFocusIn($focused, $defocused) {
     let isHandled = false;
 
     if (isNameField($defocused)) {
-        trySaveName($defocused);
+        if ($defocused.classList.contains('nameError')) {
+            $defocused.value = $defocused._original;
+            clearErrors();
+        } else {
+            trySaveName($defocused);
+        }
         isHandled = true;
     }
     if (isNameField($focused)) {
-        $focusedName = $focused;
-        $focusedName._original = $focusedName.value; // _original helps skip name save if unchanged
+        $focused._original = $focused.value; // Remember name at focus time
         isHandled = true;
     }
     return isHandled;
@@ -70,44 +70,51 @@ export function handleFocusIn($focused, $defocused) {
 
 //@ (Object) -> (Boolean), state|nil
 export async function handleInput($name) {
-    if (!isActive || $name !== $focusedName) return false;
+    if (!isActive || $name !== document.activeElement)
+        return false;
+
+    // Check name for validity, mark if invalid
     const error = await Request.checkName($name.$row._id, $name.value.trim());
     toggleError($name, error);
+
     return true;
 }
 
 //@ (Object, String) -> (Boolean), state|nil
 export function handleKeyUp($name, key) {
-    if (!isActive || $name !== $focusedName) return false;
+    if (!isActive || $name !== document.activeElement)
+        return false;
 
-    if (key === 'Enter') {
-        const success = trySaveName($name);
-        if (success) done();
-        return true;
-    }
+    if (key === 'Enter' && trySaveName($name))
+        done();
 
-    return false;
+    return true;
 }
 
 // Trim content of name field and try to save it. Return true if successful, false otherwise.
 //@ (Object) -> (Boolean), state|nil
 function trySaveName($name) {
-    const name = $name.value = $name.value.trim();
+    // Prevent save if marked invalid
+    if ($name.classList.contains('nameError'))
+        return false;
 
-    if ($name.classList.contains('nameError')) return false;
-    if (name === $name._original) return true;
+    const name =
+        $name.value = $name.value.trim();
 
+    // Skip save if unchanged
+    if (name === $name._original)
+        return true;
+
+    // Save
     const windowId = $name.$row._id;
     Request.setName(windowId, name);
     return true;
 }
 
-// On name error, add indicator and disable other fields.
-// Otherwise, remove indicator and enable other fields.
+// On name error, add error indicator.
 //@ (Object, Number) -> state
 function toggleError($name, error) {
     $name.classList.toggle('nameError', error);
-    toggleOtherFields($name, !error);
 }
 
 //@ (Boolean) -> state
@@ -117,22 +124,10 @@ function toggleDisabledActions(isDisable) {
 
 //@ (Boolean) -> state
 function toggleNameFields(isEnable) {
-    toggleFields($names, isEnable);
-}
-
-//@ (Object, Boolean) -> state
-function toggleOtherFields($field, isEnable) {
-    const $fields = [...$names];
-    $fields[$fields.indexOf($field)] = $omnibox;
-    toggleFields($fields, isEnable);
-}
-
-//@ ([Object], Boolean) -> state
-function toggleFields($fields, isEnable) {
     const tabIndex = isEnable ? 0 : -1;
     const isReadOnly = !isEnable;
-    for (const $field of $fields) {
-        $field.tabIndex = tabIndex;
-        $field.readOnly = isReadOnly;
+    for (const $name of $names) {
+        $name.tabIndex = tabIndex;
+        $name.readOnly = isReadOnly;
     }
 }
