l10nelw/win-man

Split filter.js out of omnibox.js

popup/filter.js
@@ -1,0 +1,61 @@
+import { hasClass, addClass, removeClass } from '../utils.js';
+import { $currentWindowRow, $otherWindowsList, $otherWindowRows, getName, getScrollbarWidth } from './common.js';
+
+export let $shownRows;
+
+export function init() {
+    $shownRows = $otherWindowRows;
+}
+
+// Show only rows whose names contain str, and sort them by name length, shortest first.
+export function execute(str) {
+    if (!str) {
+        $shownRows = unfilter();
+        return;
+    }
+
+    $shownRows = filter(str); // Show/hide rows and add _nameLength property to shown rows
+    if (!$shownRows.length) return;
+
+    $shownRows.sort(compareNameLength);
+    $shownRows.forEach($row => $otherWindowsList.appendChild($row)); // move filtered rows to the end of the list
+
+
+    // Add offset if scrollbar disappears
+    if (hasClass('scrollbarOffset', $currentWindowRow) && !getScrollbarWidth($otherWindowsList)) {
+        addClass('scrollbarOffset', $otherWindowsList);
+    }
+}
+
+const compareNameLength = ($a, $b) => $a._nameLength - $b._nameLength;
+
+// Hide rows whose names do not contain str, case-insensitive.
+// The rest are shown, given _nameLength property and returned as an array.
+function filter(str) {
+    str = str.toUpperCase();
+    const $filteredRows = [];
+    for (const $row of $otherWindowRows) {
+        const name = getName($row).toUpperCase();
+        const isMatch = name.includes(str);
+        $row.hidden = !isMatch;
+        if (isMatch) {
+            $row._nameLength = name.length;
+            $filteredRows.push($row);
+        }
+    }
+    return $filteredRows;
+}
+
+// Reverse all changes made by filter(): hidden rows, sort order, _index, scrollbar offset.
+// Restore sort order by comparing 'live' $otherWindowsList.children against correctly-sorted $otherWindowRows.
+function unfilter() {
+    $otherWindowRows.forEach(($correctRow, index) => {
+        $correctRow.hidden = false;
+        const $row = $otherWindowsList.children[index];
+        if ($row !== $correctRow) {
+            $otherWindowsList.insertBefore($correctRow, $row);
+        }
+    });
+    removeClass('scrollbarOffset', $otherWindowsList);
+    return $otherWindowRows;
+}

popup/omnibox.js
@@ -1,7 +1,7 @@
-import { hasClass, addClass, removeClass, toggleClass } from '../utils.js';
-import { $currentWindowRow, $otherWindowsList, $otherWindowRows, getName, getScrollbarWidth } from './common.js';
+import { removeClass, toggleClass } from '../utils.js';
 import * as Toolbar from './toolbar.js';
 import * as EditMode from './editmode.js';
+import * as Filter from './filter.js';
 import * as Request from './request.js';
 
 export const $omnibox = document.getElementById('omnibox');
@@ -21,8 +21,8 @@ export function handleKeyUp(key, event) {
     toggleClass('slashCommand', $omnibox, isSlashed);
     if (isSlashed) return handleSlashed(key, event, str, enter);
 
-    showFilteredRows(str);
-    const $firstRow = [...$otherWindowsList.children].find($row => !$row.hidden);
+    Filter.execute(str);
+    const $firstRow = Filter.$filteredRows?.[0];
     if (enter && $firstRow) Request.action(event, $firstRow);
 }
 
@@ -58,56 +58,6 @@ function handleDebugCommand(str) {
         Request.debug();
         return true;
     }
-}
-
-// Show only rows whose names contain str, and sort them by name length, shortest first.
-function showFilteredRows(str) {
-    if (!str) return showAllRows();
-
-    // Show/hide rows and add name-length values to them
-    const $filteredRows = filterRows(str);
-    if (!$filteredRows.length) return;
-
-    // Sort filtered rows and move them to the end of the list
-    $filteredRows.sort(compareNameLength);
-    $filteredRows.forEach($row => $otherWindowsList.appendChild($row));
-
-    // Add offset if scrollbar disappears
-    if (hasClass('scrollbarOffset', $currentWindowRow) && !getScrollbarWidth($otherWindowsList)) {
-        addClass('scrollbarOffset', $otherWindowsList);
-    }
-}
-
-const compareNameLength = ($a, $b) => $a._nameLength - $b._nameLength;
-
-// Hide rows whose names do not contain str, case-insensitive.
-// The rest are shown, given name-length expandos and returned as an array.
-function filterRows(str) {
-    str = str.toUpperCase();
-    const $filteredRows = [];
-    for (const $row of $otherWindowRows) {
-        const name = getName($row).toUpperCase();
-        const isMatch = name.includes(str);
-        $row.hidden = !isMatch;
-        if (isMatch) {
-            $row._nameLength = name.length;
-            $filteredRows.push($row);
-        }
-    }
-    return $filteredRows;
-}
-
-// Reverse all changes by showFilteredRows(): hidden rows, sort order, scrollbar offset.
-// Restore sort order by comparing 'live' $otherWindowsList.children against correctly sorted $otherWindowRows.
-export function showAllRows() {
-    $otherWindowRows.forEach(($correctRow, index) => {
-        $correctRow.hidden = false;
-        const $row = $otherWindowsList.children[index];
-        if ($row !== $correctRow) {
-            $otherWindowsList.insertBefore($correctRow, $row);
-        }
-    });
-    removeClass('scrollbarOffset', $otherWindowsList);
 }
 
 export function clear() {

l10nelw/winger

popup/filter.js
@@ -1,0 +1,61 @@
+import { hasClass, addClass, removeClass } from '../utils.js';
+import { $currentWindowRow, $otherWindowsList, $otherWindowRows, getName, getScrollbarWidth } from './common.js';
+
+export let $shownRows;
+
+export function init() {
+    $shownRows = $otherWindowRows;
+}
+
+// Show only rows whose names contain str, and sort them by name length, shortest first.
+export function execute(str) {
+    if (!str) {
+        $shownRows = reset();
+        return;
+    }
+
+    $shownRows = filter(str); // Show/hide rows and add _nameLength property to shown rows
+    if (!$shownRows.length) return;
+
+    $shownRows.sort(compareNameLength);
+    $shownRows.forEach($row => $otherWindowsList.appendChild($row)); // move filtered rows to the end of the list
+
+
+    // Add offset if scrollbar disappears
+    if (hasClass('scrollbarOffset', $currentWindowRow) && !getScrollbarWidth($otherWindowsList)) {
+        addClass('scrollbarOffset', $otherWindowsList);
+    }
+}
+
+const compareNameLength = ($a, $b) => $a._nameLength - $b._nameLength;
+
+// Hide rows whose names do not contain str, case-insensitive.
+// The rest are shown, given _nameLength property and returned as an array.
+function filter(str) {
+    str = str.toUpperCase();
+    const $filteredRows = [];
+    for (const $row of $otherWindowRows) {
+        const name = getName($row).toUpperCase();
+        const isMatch = name.includes(str);
+        $row.hidden = !isMatch;
+        if (isMatch) {
+            $row._nameLength = name.length;
+            $filteredRows.push($row);
+        }
+    }
+    return $filteredRows;
+}
+
+// Reverse all changes made by execute(): hidden rows, sort order, _index, scrollbar offset.
+// Restore sort order by comparing 'live' $otherWindowsList.children against correctly-sorted $otherWindowRows.
+function reset() {
+    $otherWindowRows.forEach(($correctRow, index) => {
+        $correctRow.hidden = false;
+        const $row = $otherWindowsList.children[index];
+        if ($row !== $correctRow) {
+            $otherWindowsList.insertBefore($correctRow, $row);
+        }
+    });
+    removeClass('scrollbarOffset', $otherWindowsList);
+    return $otherWindowRows;
+}

popup/omnibox.js
@@ -1,7 +1,7 @@
-import { hasClass, addClass, removeClass, toggleClass } from '../utils.js';
-import { $currentWindowRow, $otherWindowsList, $otherWindowRows, getName, getScrollbarWidth } from './common.js';
+import { removeClass, toggleClass } from '../utils.js';
 import * as Toolbar from './toolbar.js';
 import * as EditMode from './editmode.js';
+import * as Filter from './filter.js';
 import * as Request from './request.js';
 
 export const $omnibox = document.getElementById('omnibox');
@@ -21,8 +21,8 @@ export function handleKeyUp(key, event) {
     toggleClass('slashCommand', $omnibox, isSlashed);
     if (isSlashed) return handleSlashed(key, event, str, enter);
 
-    showFilteredRows(str);
-    const $firstRow = [...$otherWindowsList.children].find($row => !$row.hidden);
+    Filter.execute(str);
+    const $firstRow = Filter.$shownRows?.[0];
     if (enter && $firstRow) Request.action(event, $firstRow);
 }
 
@@ -58,56 +58,6 @@ function handleDebugCommand(str) {
         Request.debug();
         return true;
     }
-}
-
-// Show only rows whose names contain str, and sort them by name length, shortest first.
-function showFilteredRows(str) {
-    if (!str) return showAllRows();
-
-    // Show/hide rows and add name-length values to them
-    const $filteredRows = filterRows(str);
-    if (!$filteredRows.length) return;
-
-    // Sort filtered rows and move them to the end of the list
-    $filteredRows.sort(compareNameLength);
-    $filteredRows.forEach($row => $otherWindowsList.appendChild($row));
-
-    // Add offset if scrollbar disappears
-    if (hasClass('scrollbarOffset', $currentWindowRow) && !getScrollbarWidth($otherWindowsList)) {
-        addClass('scrollbarOffset', $otherWindowsList);
-    }
-}
-
-const compareNameLength = ($a, $b) => $a._nameLength - $b._nameLength;
-
-// Hide rows whose names do not contain str, case-insensitive.
-// The rest are shown, given name-length expandos and returned as an array.
-function filterRows(str) {
-    str = str.toUpperCase();
-    const $filteredRows = [];
-    for (const $row of $otherWindowRows) {
-        const name = getName($row).toUpperCase();
-        const isMatch = name.includes(str);
-        $row.hidden = !isMatch;
-        if (isMatch) {
-            $row._nameLength = name.length;
-            $filteredRows.push($row);
-        }
-    }
-    return $filteredRows;
-}
-
-// Reverse all changes by showFilteredRows(): hidden rows, sort order, scrollbar offset.
-// Restore sort order by comparing 'live' $otherWindowsList.children against correctly sorted $otherWindowRows.
-export function showAllRows() {
-    $otherWindowRows.forEach(($correctRow, index) => {
-        $correctRow.hidden = false;
-        const $row = $otherWindowsList.children[index];
-        if ($row !== $correctRow) {
-            $otherWindowsList.insertBefore($correctRow, $row);
-        }
-    });
-    removeClass('scrollbarOffset', $otherWindowsList);
 }
 
 export function clear() {